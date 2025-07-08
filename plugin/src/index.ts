0f030d4e156655bb91f0e71d8bc73c1c9b1d5987
Josep M. Blanquer <blanquer@gmail.com>
2025-07-08 20:19:05 +0200

    Pluging the expo plugin

---
+++ plugin/src/index.ts
@@ -0,0 +1,38 @@
+
+import { ConfigPlugin, createRunOncePlugin } from 'expo/config-plugins';
+
+
+const withExpoKeywordBasedRecognizer: ConfigPlugin<{
+ microphonePermission?: string;
+ speechRecognitionPermission?: string;
+ }|undefined> = (config, props) => {
+  console.log("withExpoKeywordBasedRecognizer called with props:", props);
+  if (!config.ios) {
+    config.ios = {};
+  }
+
+  if (!config.ios.infoPlist) {
+    config.ios.infoPlist = {};
+  }
+
+  config.ios.infoPlist.NSSpeechRecognitionUsageDescription =
+    props?.speechRecognitionPermission ||
+    config.ios.infoPlist.NSSpeechRecognitionUsageDescription ||
+    "This app uses speech recognition to respond to voice commands after detecting the wake word.";
+
+  config.ios.infoPlist.NSMicrophoneUsageDescription =
+    props?.microphonePermission ||
+    config.ios.infoPlist.NSMicrophoneUsageDescription ||
+    "This app uses the microphone to listen for wake words and voice commands.";
+
+  return config;
+};
+
+// export default withExpoKeywordBasedRecognizer
+
+
+export default  createRunOncePlugin(
+  withExpoKeywordBasedRecognizer,
+  "expo-keyword-based-recognizer",
+  "1.0.0",
+);
