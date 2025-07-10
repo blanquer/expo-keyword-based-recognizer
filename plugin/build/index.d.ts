863520c4997431fc731c5a53952d104d4bcf4eb1
Josep M. Blanquer <blanquer@gmail.com>
2025-07-10 18:23:15 +0200

    rebuild plugin build dir (unsure if it should be in git)

---
+++ plugin/build/index.d.ts
@@ -0,0 +1,7 @@
+import { ConfigPlugin } from 'expo/config-plugins';
+declare const withExpoKeywordBasedRecognizer: ConfigPlugin<{
+    microphonePermission?: string;
+    speechRecognitionPermission?: string;
+} | undefined>;
+export default withExpoKeywordBasedRecognizer;
+//# sourceMappingURL=index.d.ts.map
