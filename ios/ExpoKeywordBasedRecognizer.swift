90adb71e55d9edbfd7463a09b11343f27d9d2d55
Josep M. Blanquer <blanquer@gmail.com>
2025-08-11 12:21:43 -0700

    audio session settings so it plays on the speaker (instead of the receiver), and supports bluetooth I&O as well.

--- ios/ExpoKeywordBasedRecognizer.swift
+++ ios/ExpoKeywordBasedRecognizer.swift
@@ -129,7 +129,7 @@ class ExpoKeywordBasedRecognizer: NSObject {
 
   private func startAudioEngine() throws {
     let audioSession = AVAudioSession.sharedInstance()
-    try audioSession.setCategory(.playAndRecord, mode: .measurement, options: .duckOthers)
+      try audioSession.setCategory(.playAndRecord, mode: .default, options: [.allowBluetoothA2DP, .allowBluetooth, AVAudioSession.CategoryOptions.allowAirPlay, .defaultToSpeaker, .duckOthers, .overrideMutedMicrophoneInterruption])
     try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
 
     let inputNode = audioEngine.inputNode
